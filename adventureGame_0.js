4d43b0ab1e3b62ddf3fc2133d0f08ebdd0edd42e
Darlene Wagner <darlingtonwagner@gmail.com>
2025-11-28 18:58:10 -0500

    adventureGame_0.js with isDragon boolean variable

--- adventureGame_0.js
+++ adventureGame_0.js
@@ -38,7 +38,7 @@ let monsters = {
         name: 'wolfman',
         monsterDefense: 10,
         damage: 10,
-        hoard: 15,
+        hoard: 18,
         habitat: 'den in the ground',
         alive: true,
         isDragon: false
@@ -47,7 +47,7 @@ let monsters = {
         name: 'dragon',
         monsterDefense: 20,
         damage: 20,
-        hoard: 20,
+        hoard: 24,
         habitat: 'cave in a hill',
         alive: true,
         isDragon: true
@@ -250,7 +250,7 @@ function hasGoodEquipment(type){
  * Checks if player has weapon, manages combat results, and updates health
  * @returns {boolean} true if player wins, false if they retreat
  */
-function handleCombat(monsterName, monsterDefense, monsterHarm, monsterHoard, playerHealth, playerName) {
+function handleCombat(monsterName, monsterDefense, monsterHarm, monsterHoard, isDragon, playerHealth, playerName) {
 
        let monsterAlive = true;
        console.log("<- Suddenly, a " + monsterName + " creature comes up out of a hole in the ground ->");
@@ -265,6 +265,10 @@ function handleCombat(monsterName, monsterDefense, monsterHarm, monsterHoard, pl
     {
          console.log("<-                     The Battle Begins!!!                         ->");
     // Updated to check for item type instead of specific string
+    if(isDragon){
+        console.log("The Dragon bites you and you lose " + (Number(monsterHarm) - Number(bestArmor[1])) + " health points.");
+        playerHealth = updateHealth(playerHealth, Number(bestArmor[1] - monsterHarm));
+    }
     if (hasItemType("weapon")) {
         let weapon = inventory.find(item => item.type === "weapon");
         let shield = inventory.find(item => item.type === "armor");
@@ -291,7 +295,7 @@ function handleCombat(monsterName, monsterDefense, monsterHarm, monsterHoard, pl
               sleep(1000);
               playerHealth = updateHealth(playerHealth, Number(bestArmor[1] - monsterHarm));
             }
-           if(playerHealth <= 0){
+           if(playerHealth <= 0) {
                // Player must expire if health level reaches zero, this isn't a zombie game!
                console.log("You are now dead " + playerName + " and the " + monsterName + " will eat your body!"); 
                break;
@@ -596,12 +600,12 @@ function move(choiceNum) {
                         
             if(monsters.lizardman.alive) {
               //console.log(monsters.lizardman.name + " health is " + monsters.lizardman.monsterDefense);
-              [monsters.lizardman.monsterDefense, monsters.lizardman.alive, playerHealth] = handleCombat(monsters.lizardman.name, monsters.lizardman.monsterDefense, monsters.lizardman.damage, monsters.lizardman.hoard, playerHealth, playerName);
+              [monsters.lizardman.monsterDefense, monsters.lizardman.alive, playerHealth] = handleCombat(monsters.lizardman.name, monsters.lizardman.monsterDefense, monsters.lizardman.damage, monsters.lizardman.hoard, monsters.lizardman.isDragon, playerHealth, playerName);
               //console.log(monsters.lizardman.name + " health is " + monsters.lizardman.monsterDefense);
             }
             else if(monsters.wolfman.alive){
               //console.log(monsters.wolfman.name + " health is " + monsters.wolfman.monsterDefense);
-              [monsters.wolfman.monsterDefense, monsters.wolfman.alive, playerHealth] = handleCombat(monsters.wolfman.name, monsters.wolfman.monsterDefense, monsters.wolfman.damage, monsters.wolfman.hoard, playerHealth, playerName);
+              [monsters.wolfman.monsterDefense, monsters.wolfman.alive, playerHealth] = handleCombat(monsters.wolfman.name, monsters.wolfman.monsterDefense, monsters.wolfman.damage, monsters.wolfman.hoard, monsters.wolfman.isDragon, playerHealth, playerName);
               //console.log(monsters.wolfman.name + " health is " + monsters.wolfman.monsterDefense);
             }
             else if(monsters.dragon.alive){
@@ -610,7 +614,7 @@ function move(choiceNum) {
                //let weaponReady = getBestItem('weapon');
                if((hasGoodEquipment('weapon') > 1) || (hasGoodEquipment('armor') > 5)) {
                  console.log("You are sufficiently armed to fight the dragon!");
-                 [monsters.dragon.monsterDefense, monsters.dragon.alive, playerHealth] = handleCombat(monsters.dragon.name, monsters.dragon.monsterDefense, monsters.dragon.damage, monsters.dragon.hoard, playerHealth, playerName);
+                 [monsters.dragon.monsterDefense, monsters.dragon.alive, playerHealth] = handleCombat(monsters.dragon.name, monsters.dragon.monsterDefense, monsters.dragon.damage, monsters.dragon.hoard, monsters.dragon.isDragon, playerHealth, playerName);
                  console.log(monsters.dragon.name + " health is " + monsters.dragon.monsterDefense);
                }
                else{
