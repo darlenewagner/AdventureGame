c3d2c27c6e06ee91384e8ca51c43a816dcef720a
Darlene Wagner <darlingtonwagner@gmail.com>
2026-01-01 18:44:19 -0500

    Learning Data Wrangling in JavaScript

---
+++ index_html_practice/js/script4.js
@@ -0,0 +1,70 @@
+// Testing Array Operations
+const cyborgs = {
+    "borg": [{ 
+      "borgId": 101,
+      "borgJob": "Communications"
+    },
+    {
+      "borgId": 102,
+      "borgJob": "Weapons"
+    },
+    {
+       "borgId": 103,
+       "borgJob": "Assimilations"
+    }],
+
+    "tasks": [{
+        "taskId": 1,
+        "taskTitle": "Connect",
+        "borgId": 101
+    },
+    {
+        "taskId": 2,
+        "taskTitle": "Stun",
+        "borgId": 102
+    },
+    {
+        "taskId": 3,
+        "taskTitle": "Vaporize",
+        "borgId": 102
+    },
+    {
+        "taskId": 4,
+        "taskTitle": "Assimilate",
+        "borgId": 103
+    }]
+
+};
+
+
+Object.entries(cyborgs).forEach(([key, value]) => {
+ if(key === "borg"){
+    console.log(key);
+ 
+    if (Array.isArray(value)){
+    value.forEach((item, index) => {
+        console.log(`${item.borgId} does ${item.borgJob}`);
+      });
+    }
+    
+  }
+
+  
+});
+
+const drones = Object.entries(cyborgs);
+
+// const droneArray = drones.filter(([key, value]) => key === "tasks").map(([key, value]) => value);
+const droneArray = drones.filter(([key, value]) => key === "tasks").forEach(([key, value]) => {
+  if (Array.isArray(value)){
+    value.forEach((item, index) => {
+      if(item.borgId === 102) {
+        console.log(item.taskTitle);
+      }
+    });
+  }
+});
+
+
+
+console.log(droneArray);
